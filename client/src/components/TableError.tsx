3b0b023b439bc8aafa98e65837485505b73dbd36
moritzWa <wallawitsch@gmail.com>
2025-04-03 20:53:49 -0700

    rename some component

--- client/src/components/TableError.tsx
+++ client/src/components/TableError.tsx
@@ -3,7 +3,7 @@ import { AppLayout } from './AppLayout';
 import { Button } from './ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
-export const TableComponentError = ({ error }: { error: string }) => {
+export const TableError = ({ error }: { error: string }) => {
   const navigate = useNavigate();
   return (
     <AppLayout>
